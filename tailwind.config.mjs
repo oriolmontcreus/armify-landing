87adbf192089ce6e00c97286ef5d16b919a7297b
oriolmontcreus <licensedgamer641319@gmail.com>
2024-07-05 22:31:15 +0200

    feat: added framer-motion, added Flipwords component. Style changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			screens: {
-				'lg': '1075px'
-			}
+	  extend: {
+		screens: {
+		  'lg': '1075px'
 		},
+		fontFamily: {
+		  sans: ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+		},
+	  },
 	},
 	plugins: [],
-}
+  }
